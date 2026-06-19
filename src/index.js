8195a5ea7fae8e2acdf25a5a8ec804530781eb6d
Shah Mohammad Mohibbullah Shovon <mohibbullahshovon.dev@gmail.com>
2026-06-19 11:37:10 +0600

    DB MONGODB atlas connection setup practice

--- src/index.js
+++ src/index.js
@@ -1,48 +1,73 @@
 import 'dotenv/config'
-import connectDB from './db/index.js';
-import {app} from './app.js'
+import connectDB from './db/index.js'
 
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log("Server is listenting at PORT:", process.env.PORT);
-    })
-    app.on('error', ()=>{
-        console.log("Error!!!", error);
-    })
-})
-.catch((error)=>{
-    console.log("MongoDB connection Failed!!!", error);
-})
 
 
 
 
 
 
-// import {DB_NAME} from './constants'
-// import express from 'express'
-
-// const app = express();
-
-// ;( async() =>{
-
-//     try{
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error", ()=>{
-//             console.log("Error", error)
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT, ()=>{
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         })
 
 
-//     }catch(error){
-//         console.error("ERROR: ", error)
-//         throw error
-//     }
 
-// })()
+
+
+
+// import connectDB from './db/index.js';
+// import {app} from './app.js'
+
+
+// connectDB()
+// .then(()=>{
+//     app.listen(process.env.PORT || 8000, ()=>{
+//         console.log("Server is listenting at PORT:", process.env.PORT);
+//     })
+//     app.on('error', ()=>{
+//         console.log("Error!!!", error);
+//     })
+// })
+// .catch((error)=>{
+//     console.log("MongoDB connection Failed!!!", error);
+// })
+
+
+
+
+
+/*
+
+import express from 'express';
+import mongooes from 'mongoose';
+import { DB_NAME } from './constants';
+
+
+const app = express();
+
+
+// using IIFE(immediately invoked function expression) method to connect to the database using mongoose
+
+;(async ()=>{
+    try {
+        await mongooes.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error)=> console.error("The error was about listening the server please check the connection codebase. The error is :", error))
+
+        app.listen(process.env.PORT || 8000, ()=>{
+            console.log("The Server/App is listening at port : ", process.env.PORT || 8000)
+        })
+
+    } catch (error) {
+        console.error("Database connection error: ",error);
+    }
+})()
+
+
+
+*/
+
+
+
+
+
