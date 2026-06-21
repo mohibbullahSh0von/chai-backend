db5950b0df074c2e4afd2004480c09ee4a023ecb
Shah Mohammad Mohibbullah Shovon <mohibbullahshovon.dev@gmail.com>
2026-06-21 10:23:36 +0600

    added user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
+const videoSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number, // cloudnary URL
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: true
+    },
+    isPublished: {
+        type: Boolean,
+        required: true,
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    thumbnail:{
+        type: String, //cloudnary url
+        required: true,
+    },
+    videoFile:{
+        type: String, //cloudnary url
+        required: true
+    }
+}, {timestamps: true});
+
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+
+export const Video = mongoose.model("Video", videoSchema);
